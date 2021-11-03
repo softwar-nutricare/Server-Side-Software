8765bd012f1c206e53165004d6d2485a8710b105
Josue Cartagena <zectaros@hotmail.com>
2021-11-02 23:00:29 -0500

    Initial Commit

---
+++ src/main/java/pe/edu/upc/NutriCare_API/NutriCareApiApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.NutriCare_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriCareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriCareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/NutriCare_API/NutriCareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.NutriCare_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriCareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
